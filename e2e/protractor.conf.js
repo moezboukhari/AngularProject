5182287196eb92dc619c917cfc384a69d864346e
root <root@localhost.localdomain>
2018-09-05 17:25:37 +0100

    re

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -2,14 +2,7 @@
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
 const { SpecReporter } = require('jasmine-spec-reporter');
-const config = require('./protractor.conf').config;
 
-config.capabilities = {
-  browserName: 'chrome',
-  chromeOptions: {
-    args: ['--headless', '--no-sandbox', '--disable-gpu']
-  }
-};
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
