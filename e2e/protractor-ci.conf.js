83453ed55902fbe383f9b5532f6675ed48eb683d
root <root@localhost.localdomain>
2018-09-05 22:37:02 +0100

    re

--- e2e/protractor-ci.conf.js
+++ e2e/protractor-ci.conf.js
@@ -3,7 +3,7 @@ const config = require('./protractor.conf').config;
 config.capabilities = {
   browserName: 'chrome',
   chromeOptions: {
-    args: ['--headless', '--disable-gpu']
+    args: ['--headless', '--no-sandbox=false', '--disable-gpu']
   }
 };
 
