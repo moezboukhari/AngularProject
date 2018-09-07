773fe94d7a706423387d9819315061a7be579ed3
root <root@localhost.localdomain>
2018-09-07 03:46:31 +0100

    re

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -1,7 +1,7 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-const { SpecReporter } = require("jasmine-spec-reporter");
+const { SpecReporter } = require('jasmine-spec-reporter');
 
 exports.config = {
   allScriptsTimeout: 11000,
@@ -9,8 +9,10 @@ exports.config = {
     './src/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    {browserName: "Firefox"},
-    {browserName: "chrome"}
+    browserName: 'Firefox'
+  },
+  capabilities: {
+    browserName: 'chrome'
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
