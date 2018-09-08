552e336a72f2acd61fecb4ce2d8d3bd9e32abdd2
moezboukhari <moez.boukhari@gmail.com>
2018-09-09 00:59:33 +0100

    feat(ep18): angular routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { DashboardRoutingModule } from './dashboard/dashboardRouteModule';
+import { AppRoutingModule } from './app.routes';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 import { AppComponent } from './app.component';
+import { FooDirective } from './foo.directive';
+import { CapitalisePipe } from './capitalise.pipe';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, FooDirective, CapitalisePipe, PageNotFoundComponent],
+  imports: [BrowserModule, DashboardModule, DashboardRoutingModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/app.routes.ts
@@ -0,0 +1,19 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+  // { path: 'path/:routeParam', component: MyComponent },
+  // { path: 'staticPath', component: ... },
+  // { path: '**', component: ... },
+  // { path: 'oldPath', redirectTo: '/staticPath' },
+  // { path: ..., component: ..., data: { message: 'Custom' }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
